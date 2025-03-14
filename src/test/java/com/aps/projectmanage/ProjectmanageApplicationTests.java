4680572535a6516537aaa588eaed705e63d8f407
hoangson2004 <lehoangson022004>
2025-03-14 14:10:57 +0700

    reinitial project

---
+++ src/test/java/com/aps/projectmanage/ProjectmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.projectmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
